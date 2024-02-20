7013b9c643ba1ecc39ec8d2ebd2e325bb900fb05
Diego Torres <126494563+diegot-code@users.noreply.github.com>
2024-02-20 16:59:37 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+def main():
+    def math(value1, value2, operation): #Perform basic arithmetic operations.
+        print(value1, value2, operation)
+        # Perform corresponding calculation based on the operation: '+', '-', '*', '/', or print an error for invalid operation
+
+    # Get user input for the first and second values
+    # Get user input for the desired arithmetic operation
+    
+    # Call the 'math' function and print the result
+
+if __name__ == "__main__":
+    main()
