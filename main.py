0915a8877520f1995af0d2845e20ddf23844cf46
Diego Torres <126494563+diegot-code@users.noreply.github.com>
2024-06-24 13:33:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-def main():
-    def math(value1, value2, operation): #Perform basic arithmetic operations.
+def math(value1, value2, operation): #Perform basic arithmetic operations.
         print(value1, value2, operation)
         # Perform corresponding calculation based on the operation: '+', '-', '*', '/', or print an error for invalid operation
+
+
+def main():
+    
 
     # Get user input for the first and second values
     # Get user input for the desired arithmetic operation
